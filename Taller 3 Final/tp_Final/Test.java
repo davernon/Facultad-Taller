830af93c05de6ba33803d61a97a77e87540c4391
davernon <39773554+davernon@users.noreply.github.com>
2018-05-30 21:51:49 -0300

    Update Test.java

--- Taller 3 Final/tp_Final/Test.java
+++ Taller 3 Final/tp_Final/Test.java
@@ -5,6 +5,37 @@ public class Test {
 	public static void main(String[] args) {
 		
 
+		/*
+		 * 1- Ejecutar "abrirPrograma()"
+		 * 	a- conecta a la DB
+		 * 	b- instancia un scanner para tipear
+		 * 	c- muestra el menu principal 
+		 * 
+		 * 2- Seleccionar "crear torneo()"
+		 * 	a- se ejecuta el metodo "llamarTodosJugadores()" que levanta a los 16 que va a haber en la db
+		 * 	b- se agregan a la cola   
+		 * 	c- se instancia un timer
+		 * 	d- se instancian 8 mesas y se suscriben al timer.
+		 * 
+		 * 3- Se ejecuta "armarRonda()"
+		 *  a- se recorre la cola de jugadores y se guarda en un aux
+		 *  b- Se pregunta si jugador1 o 2 está en "null". Si es así, mete al jugador ahi. Si no, va a la próxima mesa.
+		 *  
+		 * 
+		 * 4- Se ejecuta "mostrarJugadoresXMesa()"
+		 *  a- Recorre las mesas y muestra jugador1 y jugador2
+		 * 
+		 * 5- Se ejecuta "finalizarRonda()"
+		 *  a- se cambia el estado  de timer
+		 *  b- se dispara "notify()"
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
 	}
 	
 	
